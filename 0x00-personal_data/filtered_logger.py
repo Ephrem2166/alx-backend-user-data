fb0497c0d853489607b629462a657f0716e1a4da
Ephrem2166 <ephrem2181@gmail.com>
2023-08-31 10:53:31 +0000

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -113,12 +113,12 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
            - Use the module mysql-connector-python to connect to the MySQL
              database (pip3 install mysql-connector-python)
     '''
-    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
 
-    connection_db = mysql.connector.connection.MySQLConnection(user=username,
+    connection_db = mysql.connector.connect(user=username,
                                                      password=password,
                                                      host=host,
                                                      database=db_name)
